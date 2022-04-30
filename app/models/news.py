7a0e3306f7fb9d72f81f2f926b53383efdf5dbea
Olliemint <furywrld5@gmail.com>
2022-04-30 21:06:14 +0300

    adding object constructor

---
+++ app/models/news.py
@@ -0,0 +1,23 @@
+class Articles:
+    
+    def __init__(self,title,author,description,link,image,publishedAt):
+        
+        self.title = title
+        self.author = author
+        self.description = description
+        self.link = link
+        self.image = image
+        self.publishedAt = publishedAt
+    
+    
+class Sources:
+    
+    def __init__(self,name,description,url,category,country):
+        
+        self.name = name
+        self.description =description
+        self.url = url
+        self.category = category
+        self.country = country
+        
+                   
---
+++ app/request.py
@@ -0,0 +1,4 @@
+from app import app
+from instance.config import News_api
+
+api_key = app.config["News_api"]
