62ac724c4f76358c8f87d50daac91bf468133871
Olliemint <furywrld5@gmail.com>
2022-05-01 16:22:58 +0300

    adding function to get news

--- app/request.py
+++ app/request.py
@@ -1,4 +1,37 @@
 from app import app
-from instance.config import News_api
 
+
+import urllib.request,json
+
+from .models import news
+
+
+News = news.News
+
+
+#getting api key
 api_key = app.config["News_api"]
+
+#getting news url
+main_url = app.config["news_base_url"]
+
+
+def get_article(category):
+    
+    get_news_url = main_url.format(category,api_key)
+    
+    with urllib.request.urlopen(get_news_url) as url:
+        
+        get_news_data = url.read()
+        
+        get_news_response = json.loads(get_news_data)
+        
+        news_results = None
+        
+        if get_news_response["results"]:
+            
+            news_results_list = get_news_response["results"]
+            
+            news_results = process_results(news_results_list)
+        
+    return news_results    
