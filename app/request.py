702f5f8e4a50f5daa92040880501e8708e7dea36
Olliemint <furywrld5@gmail.com>
2022-05-03 01:51:59 +0300

    function that process results to list of objects

--- app/request.py
+++ app/request.py
@@ -75,6 +75,28 @@ def process_results(news_list):
 
 def get_sources(category):
     
+    get_sources_url = source_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        
+        get_sources_response = json.loads(get_sources_data)
+        
+        source_results = None
+        if get_sources_response['sources']:
+            
+            source_results_list = get_sources_response['sources']
+            source_results = process_sources(source_results_list)
+    
+    return source_results        
+
+def process_sources(source_list):
+    """
+    function transform to list objects
+    
+    """  
+    
+    source_results = []          
           
 
         
