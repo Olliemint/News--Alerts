5e82400e1afc7a6a413e2ec633b9b4aff48c3e0d
Olliemint <furywrld5@gmail.com>
2022-05-03 01:30:43 +0300

    passed parameters to class Source

--- app/models/source.py
+++ app/models/source.py
@@ -2,3 +2,9 @@ class Source:
     """
     source class to define source objects
     """
+    
+    def __init__(self,name,description,url):
+        
+        self.name = name
+        self.description = description
+        self.url = url
